5e21c792dafe47c30189d38e5978f4341ab0be6d
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-05-23 21:41:52 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
 
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
+const path = require('path');
+const files = path.join(__dirname, 'files');
+const copy = path.join(__dirname, 'files-copy');
+
+const init = async () => {
+  await rm(copy, { recursive: true, force: true });
+  await mkdir(copy);
+  const fileNames = await readdir(files);
+  await Promise.all(
+    fileNames.map(n => copyFile(path.join(files, n), path.join(copy, n)))
+  );
+}
+
+init();
