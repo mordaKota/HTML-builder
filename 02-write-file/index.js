04ac16847a6cd0502b976b4947922f42ff080ed1
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-05-23 13:39:45 +0300

    feat: done 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const output = fs.createWriteStream(path.join(__dirname, 'text2.txt'));
+
+const exit = () => {
+  process.stdout.write('The program will be closed. Buy!');
+  process.exit();
+}
+
+process.stdout.write('Please add some text to save or type "exit" to close \n');
+process.stdin.on('data', chunk => {
+  chunk.toString().trim() === 'exit' ? exit() : output.write(chunk)
+});
+
+process.on('SIGINT', exit);
+
