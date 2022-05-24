fbce16ec5a8ac9ee2a253a09d5d8da26b4eeaa4c
Ekaterina Danilova <danilova.ek.an@gmail.com>
2022-05-24 15:16:31 +0300

    fix: fix output format

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,19 +4,19 @@ const path = require('path');
 const secretPath = path.join(__dirname, 'secret-folder');
 console.log(secretPath);
 
-fs.readdir(secretPath, (err, files) => {
+fs.readdir(secretPath, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.log(err);
   } else {
     console.log("\nSecret Folder Files:");
     files.forEach(file => 
       {
-        fs.stat(path.join(secretPath, file), (err, stats) => {
+        fs.stat(path.join(secretPath, file.name), (err, stats) => {
           if (err) {
             console.log(err);
           } 
           if (!stats.isDirectory()) {
-            console.log(file);
+            console.log(file.name.split('.').slice(0, -1).join('.') + " - " + path.extname(file.name).slice(1) + " - " + ((Math.round(stats.size/1024)) + 'Kb'));
           }
         })
         
